cf76df227589bcd8d1a05b76fb4fed60287a3721
Dennis Lahay <dlahay@gmail.com>
2016-12-01 11:22:22 -0600

    LocationsGrid content. Added some scss variables.

--- src/components/LocationsGrid/LocationsGridBox.js
+++ src/components/LocationsGrid/LocationsGridBox.js
@@ -5,14 +5,15 @@ import { getTimeZone } from '../../helpers/timezone.js';
 export class LocationsGridBox extends Component {
     static propTypes = {
         city: PropTypes.string,
-        tz: PropTypes.string
+        tz: PropTypes.string,
+        handleMouseOverBox: PropTypes.func
     }
 
     render() {
-        const { city, tz, time } = this.props;
+        const { index, city, tz, time } = this.props;
 
         return (
-            <div className="locations-grid__box">
+            <div className="locations-grid__box" onMouseOver={(e) => this.props.handleMouseOverBox(index)}>
                 <div className="locations-grid__city">{city}</div>
                 <div className="locations-grid__time">{getTimeZone(time,`${tz}`)}</div>
             </div>
